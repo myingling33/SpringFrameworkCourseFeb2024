408717bf938990666ec2a7914972bb3f630cbad5
Michael Yingling <myingling33@gmail.com>
2024-02-19 06:58:12 -0600

    example6 - demo of @PostConstruct

---
+++ example6/src/main/java/com/example/main/Example6.java
@@ -0,0 +1,17 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example6 {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        // Previously, the line below generated a null, but with PostConstruct, this solves that issue.
+        System.out.println("Component Vehicle name from Spring Context is: " + vehicle.getName()); // Component Vehicle name from Spring Context is: Honda
+        vehicle.printHello(); // Printing Hello from Component Vehicle Bean
+    }
+}
