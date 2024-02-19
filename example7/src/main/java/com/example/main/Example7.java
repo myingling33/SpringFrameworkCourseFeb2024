03e2ebd622c501472b2b4f6207dac64c3350117c
Michael Yingling <myingling33@gmail.com>
2024-02-19 07:06:51 -0600

    example6 - demo of @PreDestroy

--- example7/src/main/java/com/example/main/Example7.java
+++ example7/src/main/java/com/example/main/Example7.java
@@ -4,7 +4,7 @@ import com.example.beans.Vehicle;
 import com.example.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Example6 {
+public class Example7 {
 
     public static void main(String[] args) {
 
@@ -13,6 +13,5 @@ public class Example6 {
         // Previously, the line below generated a null, but with PostConstruct, this solves that issue.
         System.out.println("Component Vehicle name from Spring Context is: " + vehicle.getName()); // Component Vehicle name from Spring Context is: Honda
         vehicle.printHello(); // Printing Hello from Component Vehicle Bean
-        context.close(); // Destroying Vehicle Bean
     }
 }
