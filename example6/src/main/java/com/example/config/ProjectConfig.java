cd833851cb9ec2c7da3710135290184c1434ce52
Michael Yingling <myingling33@gmail.com>
2024-02-19 06:58:34 -0600

    example6 - demo of @PostConstruct

---
+++ example6/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,16 @@
+package com.example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/*
+Spring @Configuration annotation is part of the spring core framework.
+Spring Configuration annotation indicates that the class has @Bean
+definition methods. So, Spring container can process the class and generate
+Spring Beans to be used in the application.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.example.beans")
+public class ProjectConfig {
+
+}
