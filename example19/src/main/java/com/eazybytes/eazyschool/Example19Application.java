fc9fdbd00f8c86eeadfc38fc646cbfa399e5c3ea
Michael Yingling <myingling33@gmail.com>
2024-02-24 06:51:38 -0600

    example19 - generated skeleton web app

---
+++ example19/src/main/java/com/eazybytes/eazyschool/Example19Application.java
@@ -0,0 +1,13 @@
+package com.eazybytes.eazyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example19Application.class, args);
+	}
+
+}
