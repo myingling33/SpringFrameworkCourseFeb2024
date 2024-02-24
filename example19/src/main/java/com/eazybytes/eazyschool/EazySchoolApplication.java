6c44b01633c38a56950f7e70fe2b46e2ecab34d0
Michael Yingling <myingling33@gmail.com>
2024-02-24 07:43:30 -0600

    example19 - demo of Spring Boot Autoconfiguration

--- example19/src/main/java/com/eazybytes/eazyschool/EazySchoolApplication.java
+++ example19/src/main/java/com/eazybytes/eazyschool/EazySchoolApplication.java
@@ -3,6 +3,7 @@ package com.eazybytes.eazyschool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @SpringBootApplication
 public class EazySchoolApplication {
 
