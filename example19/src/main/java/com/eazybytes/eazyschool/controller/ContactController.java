c819e2bcd2648c73d8ed2f10741b5e30888faf6f
Michael Yingling <myingling33@gmail.com>
2024-02-25 09:13:03 -0600

    example19 - implemented @Slf4j

--- example19/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ example19/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -2,18 +2,18 @@ package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.service.ContactService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class ContactController {
 
-    private static Logger log = LoggerFactory.getLogger(ContactController.class);
+    // private static Logger log = LoggerFactory.getLogger(ContactController.class);
 
     private final ContactService contactService;
 
