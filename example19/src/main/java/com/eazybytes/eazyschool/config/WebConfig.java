dd61106a1f4e40a01356fd5b39121f4c4b62425d
Michael Yingling <myingling33@gmail.com>
2024-02-25 07:35:50 -0600

    example19 - built about page

--- example19/src/main/java/com/eazybytes/eazyschool/config/WebConfig.java
+++ example19/src/main/java/com/eazybytes/eazyschool/config/WebConfig.java
@@ -7,8 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // static pages
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/courses").setViewName("courses");
+        registry.addViewController("/about").setViewName("about");
     }
 }
