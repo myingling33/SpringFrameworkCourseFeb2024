be48435582f2e4c5b7fec0eafa65f5a25471d62b
Michael Yingling <myingling33@gmail.com>
2024-02-28 05:42:53 -0600

    example19 - demo of @RequestScope added

--- example19/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
+++ example19/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -3,10 +3,18 @@ package com.eazybytes.eazyschool.service;
 import com.eazybytes.eazyschool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Slf4j
 @Service
+@RequestScope
 public class ContactService {
+
+    private int counter = 0;
+
+    public ContactService() {
+        System.out.println("Contact Service Bean initialized");
+    }
 
     // private static Logger log = LoggerFactory.getLogger(ContactService.class);
 
@@ -20,5 +28,13 @@ public class ContactService {
         //TODO - Need to persist the data into the DB table
         log.info(contact.toString());
         return isSaved;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 }
