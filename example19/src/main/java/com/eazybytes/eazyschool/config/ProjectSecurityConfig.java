52b005b2de472b4433b8b6c47382e594a2330deb
Michael Yingling <myingling33@gmail.com>
2024-02-29 08:50:11 -0600

    example19 - implement login/logout in ProjectSecurityConfig

--- example19/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example19/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -16,16 +16,20 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(requests -> requests
-                .requestMatchers("/", "/home").permitAll()
-                .requestMatchers("/holidays/**").permitAll()
-                .requestMatchers("/contact").permitAll()
-                .requestMatchers("/saveMsg").permitAll()
-                .requestMatchers("/courses").permitAll()
-                .requestMatchers("/about").permitAll()
-                .requestMatchers("/assets/**").permitAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+                .authorizeHttpRequests(requests -> requests.requestMatchers("/dashboard").authenticated()
+                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/holidays/**").permitAll()
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/saveMsg").permitAll()
+                        .requestMatchers("/courses").permitAll()
+                        .requestMatchers("/about").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/assets/**").permitAll())
+                        .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
+                                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
+                        .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
+                                .invalidateHttpSession(true).permitAll())
+                        .httpBasic(Customizer.withDefaults());
 
        return http.build();
     }
