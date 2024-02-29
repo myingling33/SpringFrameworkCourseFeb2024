6649fe690c7dcdf2ada1b8c62cb7ef03f2a92909
Michael Yingling <myingling33@gmail.com>
2024-02-29 08:09:12 -0600

    example19 - practice with CSRF protection & CSRF disable

--- example19/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example19/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -12,7 +12,8 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(requests -> requests
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/", "/home").permitAll()
                 .requestMatchers("/holidays/**").permitAll()
                 .requestMatchers("/contact").permitAll()
